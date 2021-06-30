f55db628a2e7cc8a008d28862c36bf6add4e7c5e
Chase <cabbott93@gmail.com>
2021-06-30 11:42:02 -0700

    passing post new post route

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -5,22 +5,23 @@ import app from '../lib/app.js';
 
 describe('RESTful routes for user posts', () => {
   let user;
+  let agent;
   beforeAll(async () => {
-    user = { email: 'cabbott93@gmail.com', password: 'hello' };
-    await request.agent(app).post('/api/auth/signup').send(user);
+    agent = request.agent(app);
+    user = { email: 'cabbott94@gmail.com', password: 'hello' };
+    await agent.post('/api/auth/signup').send(user);
     return setup(pool);
   });
 
   it('adds a new post associated with a user', async () => {
     const post = {
-      user: user.email,
       photoUrl: 'www.me.com/me',
       caption: 'look at me',
       tags: ['sun', 'summer']
     };
 
-    const { body } = await request.agent(app).post('/api/posts').send(post);
+    const { body } = await agent.post('/api/posts').send(post);
 
-    expect(body).toEqual({ id: '1', ...post });
+    expect(body).toEqual({ id: '1', userId: '2', ...post });
   });
 });
